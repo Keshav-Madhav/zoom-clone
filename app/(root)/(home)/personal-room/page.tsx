9a5e4cef3b40e043ade712398faed38abaf715cd
Keshav Madhav <keshav2552003@gmail.com>
2024-06-15 20:38:11 +0530

    Added pages for nav bar links

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const PersonalRoom = (props: Props) => {
+  return (
+    <div>PersonalRoom</div>
+  )
+}
+
+export default PersonalRoom
---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Previous
+ = (props: Props) => {
+  return (
+    <div>Previous
+      
+    </div>
+  )
+}
+
+export default Previous
